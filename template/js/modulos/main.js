2603b2a216faef8b831fa2b790212f5d5f2647fe
hchenalonzo84 <hugrishoo@gmail.com>
2024-03-28 17:55:32 -0600

    version1.0

---
+++ template/js/modulos/main.js
@@ -0,0 +1,7 @@
+import hamburgerMenu from "../modulos/menu-hamburguesa.js";
+
+const d=document;
+
+d.addEventListener("DOMContentLoaded",(e)=>{
+    hamburgerMenu(".panel-btnPerso", ".panelperso",".menuPerso a");  
+});
