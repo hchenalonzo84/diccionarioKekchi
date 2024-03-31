c84b55c9ffc9a3381af5c524a1a7a1dcac4f5c86
hchenalonzo84 <hugrishoo@gmail.com>
2024-03-31 12:29:16 -0600

    version1.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # diccionarioKekchi
 Diconario  de español a idiom Maya Kekchi
+
+integrantes del proyecto:
+
+Hugo Rolando Chenal Alonzo 0902-12-3918.
+
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	// "fmt"
 
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./template"))
+	http.Handle("/", fs)
+	log.Print("escuchando en el puerto 3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
